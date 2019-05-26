519cf93711f498618fcf840208ecd140f7cb0c63
Fatima <34818034+FatimaVelic@users.noreply.github.com>
2019-05-26 16:19:55 +0200

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProject1.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string PTitle { get; set; }
+        public string PDescription { get; set; }
+        public string PColor { get; set; }
+        public string PSize { get; set; }
+        public string PCategory{ get; set; }
+        public string PStatus { get; set; }
+        public double PPrice { get; set; }
+    }
+}
---
+++ WebAppProject1Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProject1.Models
+{
+    public class WebAppProject1Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WebAppProject1Context() : base("name=WebAppProject1Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebAppProject1.Models.Cart2> Cart2 { get; set; }
+    }
+}
